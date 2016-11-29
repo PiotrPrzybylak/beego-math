74a2bb8c1d31e3ac6dbde5a9830e144a5d35ffdd
Piotr Przybylak <piotr.przybylak@rndity.com>
2016-11-29 14:13:18 +0100

    deply scripts

--- update.sh
+++ update.sh
@@ -2,5 +2,7 @@
 
 docker pull $1/ma-prod:latest
 if docker stop ma-app; then docker rm ma-app; fi
+echo "running image"
 docker run -d -p 8080:8080 --name ma-app $1/ma-prod
+echo "running docker rmi"
 if docker rmi $(docker images --filter "dangling=true" -q --no-trunc); then :; fi
